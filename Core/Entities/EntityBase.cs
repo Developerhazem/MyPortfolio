00f8190dae1211c43d0f65266bc1f3a050d7fcce
Zozo <eng.hazem.k@hotmail.com>
2020-08-29 16:02:54 +0300

    Add project files.

---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.Entities
+{
+    public class EntityBase
+    {
+        //Guid data type is like int, we make it in separate class to remove duplicate code in each class...
+        public Guid Id { get; set; }
+    }
+
+}
